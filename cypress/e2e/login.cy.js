9719783ab3fd41a7efeb4535277b633ba43cdf12
Fernando Papito <papitorocks@gmail.com>
2023-03-22 17:49:03 -0300

    custom commands

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -9,19 +9,7 @@ describe('login', () => {
     context('quando submeto o formulário', () => {
         it.only('deve logar com sucesso', () => {
             const user = data.success
-
-            cy.task('removeUser', user.email)
-                .then(function(result){
-                    cy.log(result)
-                })
-
-            cy.request({
-                method: 'POST',
-                url: 'http://localhost:3333/users',
-                body: user
-            }).then(function(response){
-                expect(response.status).to.eq(201)
-            })
+            cy.createUser(user)
 
             loginPage.submit(user.email, user.password)
             shaversPage.header.userShouldBeLoggedIn(user.name)            
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,19 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+Cypress.Commands.add('createUser', (user) => {
+    cy.task('removeUser', user.email)
+        .then(function (result) {
+            cy.log(result)
+        })
+
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:3333/users',
+        body: user
+    }).then(function (response) {
+        expect(response.status).to.eq(201)
+    })
+})
